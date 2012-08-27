0d7ceaa8664bfd22f22dfb4e07510958cedf8ec1
Markus Ernst <ernst.sukram@gmail.com>
2012-08-27 01:52:24 -0500

    cycle through the color changes

--- src/CatPictureApp.cpp
+++ src/CatPictureApp.cpp
@@ -1,5 +1,7 @@
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
+#include <cstdlib> 
+#include <iostream>
 
 using namespace ci;
 using namespace ci::app;
@@ -11,24 +13,82 @@ class CatPictureApp : public AppBasic {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+
+  private:
+	float color1;
+	float color2;
+	float color3;
+	int iteration;
 };
 
 void CatPictureApp::setup()
 {
-}
+	color1 = 1.0;
+	color2 = 1.0;
+	color3 = 1.0;
+	iteration = 0;
+};
 
 void CatPictureApp::mouseDown( MouseEvent event )
 {
-}
+};
 
 void CatPictureApp::update()
 {
-}
+	if(color1 != 0.0){
+	color1 = color1 - 0.015;
+	};
+
+	//if(color1 < 0.0){
+	//	color1 = 1.0;
+	//};
+
+	if(color1 < 0.0 && color2 != 0.0){
+	color2 = color2 - 0.015;
+	};
+
+	if(color1 < 0.0 && color2 < 0.0 && color3 != 0.0){
+	color3 = color3 - 0.015;
+	};
+	
+	if(color1 < 0.0 && color2 < 0.0 && color3 < 0.0){
+	color1 = 1.0;
+	color2 = 1.0;
+	color3 = 1.0;
+	iteration = iteration + 1;
+	if(iteration == 6){
+		iteration = 0;
+	};
+	};
+
+};
 
 void CatPictureApp::draw()
 {
 	// clear out the window with black
-	gl::clear( Color( 1.0, 1.0, 0 ) ); 
-}
+	if(iteration == 0){
+	gl::clear( Color( color1, color2, color3 ) ); 
+	};
+
+	if(iteration == 3){
+	gl::clear( Color( color1, color3, color2 ) ); 
+	};
+
+	if(iteration == 1){
+	gl::clear( Color( color2, color1, color3 ) ); 
+	};
+
+	if(iteration == 4){
+	gl::clear( Color( color3, color1, color2 ) ); 
+	};
+
+	if(iteration == 2){
+	gl::clear( Color( color2, color3, color1 ) ); 
+	};
+
+	if(iteration == 5){
+	gl::clear( Color( color3, color2, color1 ) ); 
+	};
+};
 
 CINDER_APP_BASIC( CatPictureApp, RendererGl )
